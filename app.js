4b352b51185bbf6acdd447b1c8acadd23d92a5a0
Thragull <146970246+Thragull@users.noreply.github.com>
2024-01-18 12:05:56 +0000

    reto superado

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-let pronoun = ['the', 'our','my', 'their', 'her', 'his', 'your', 'a', ];
-let adj = ['great', 'big', 'amazing', 'awesome', 'ultimate'];
-let noun = ['jogger', 'racoon', 'squirrel', 'grizzly', 'otter', 'beaver'];
-let extension = ['.com', '.es', '.net', '.org', '.us', '.co.uk', '.io'];
+let pronoun = ['the', 'our','my', 'their', 'her', 'his', 'your'];
+let adj = ['great', 'big', 'amazing', 'awesome', 'ultimate', 'LAST', 'best'];
+let noun = ['sitcom', 'mystic', 'planet', 'scenario', 'cyborg', 'whises', 'ofus'];
+let extension = ['com', 'es', 'net', 'org', 'us', 'co.uk', 'io'];
 
 let domain = '';
 let counter = 1;
@@ -9,7 +9,15 @@ for (let i=0; i<pronoun.length; i++){
     for (let j=0; j<adj.length; j++){
         for (let k=0; k<noun.length; k++){
             for (let l=0; l<extension.length; l++){
-                console.log(counter+' '+pronoun[i]+adj[j]+noun[k]+extension[l]);
+                domain = pronoun[i]+adj[j]+noun[k];
+                if (domain.endsWith(extension[l])){
+                    domain = domain.slice(0,-extension[l].length);
+                    domain += '.'+extension[l];
+                }
+                else {
+                    domain+= '.'+extension[l];
+                }
+                console.log(counter+' '+domain);
                 counter++;
             }
         }
